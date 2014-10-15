8cbcdca4c1d59508f93b426a5b401c0fa0a5c106
lauyikonn <lau.yikonn@gmail.com>
2014-10-15 15:11:54 +0800

    commit

--- build/android/assets/alloy/styles/main.js
+++
@@ -1,1 +0,0 @@
-module.exports=[{isApi:!0,priority:1000.0038,key:"Label",style:{width:Ti.UI.SIZE,height:Ti.UI.SIZE,color:"#000"}},{isClass:!0,priority:10000.0037,key:"container",style:{backgroundColor:"white"}}];
--- build/android/assets/alloy/widgets/nl.fokkezb.drawer/controllers/widget.js
+++
@@ -1,1 +0,0 @@
-/Users/geomilano/Documents/Titanium_Studio_Workspace/SissonsPaint/Resources/android/alloy/widgets/nl.fokkezb.drawer/controllers/widget.js
