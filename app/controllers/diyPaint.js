04949f05e49907ac5c402ea90bc409f1ec5e0fe7
lauyikonn <lau.yikonn@gmail.com>
2014-10-21 10:11:58 +0800

    DIY photo module

--- app/controllers/diyPaint.js
+++ app/controllers/diyPaint.js
@@ -3,25 +3,23 @@ var pWidth = Ti.Platform.displayCaps.platformWidth;
 var pHeight = PixelsToDPUnits(Ti.Platform.displayCaps.platformHeight);
 var toolbarHeight = $.toolbar.rect.height;
 var toggleHeight = $.toggle.getHeight();
+var canvasHeight = 0;
 
 $.toolbar.addEventListener('postlayout', function(e) {
 	console.log(pWidth);
 	console.log(PixelsToDPUnits(pWidth));
 	toolbarHeight = $.toolbar.rect.height;
-	var canvasHeight = pHeight - toolbarHeight - 48 - toggleHeight;
+	canvasHeight = pHeight - toolbarHeight - 25 - toggleHeight;
+	console.log("page height: "+pHeight+", toolbar:"+toolbarHeight+", toggle"+toggleHeight+", canvas:"+canvasHeight);
 	$.canvas.setBottom(toolbarHeight);
 	$.canvas.setHeight(canvasHeight);
 });
 	
-Ti.API.info('Ti.Platform.displayCaps.density: ' + Ti.Platform.displayCaps.density);
-Ti.API.info('Ti.Platform.displayCaps.dpi: ' + Ti.Platform.displayCaps.dpi);
-Ti.API.info('Ti.Platform.displayCaps.platformHeight: ' + Ti.Platform.displayCaps.platformHeight);
-Ti.API.info('Ti.Platform.displayCaps.platformWidth: ' + Ti.Platform.displayCaps.platformWidth);
-if(Ti.Platform.osname === 'android'){
-  Ti.API.info('Ti.Platform.displayCaps.xdpi: ' + Ti.Platform.displayCaps.xdpi);
-  Ti.API.info('Ti.Platform.displayCaps.ydpi: ' + Ti.Platform.displayCaps.ydpi);
-  Ti.API.info('Ti.Platform.displayCaps.logicalDensityFactor: ' + Ti.Platform.displayCaps.logicalDensityFactor);
-}
+$.canvas.addEventListener("load", function(){
+	console.log(canvasHeight);
+	console.log(pWidth);
+	Ti.App.fireEvent('web:initCanvasSize', { height: canvasHeight, width: pWidth });
+});	
 
 function PixelsToDPUnits(ThePixels)
 {
@@ -33,3 +31,96 @@ function DPUnitsToPixels(TheDPUnits)
 {
   return (TheDPUnits * (Titanium.Platform.displayCaps.dpi / 160));
 }
+
+
+//Create a dialog with options
+var dialog = Titanium.UI.createOptionDialog({
+    //title of dialog
+    title: 'Choose an image source...',
+    //options
+    options: ['Camera','Photo Gallery', 'Cancel'],
+    //index of cancel button
+    cancel:2
+});
+ 
+//add event listener
+dialog.addEventListener('click', function(e) {
+    //if first option was selected
+    if(e.index == 0)
+    {
+        //then we are getting image from camera
+        Titanium.Media.showCamera({
+            //we got something
+            success:function(event)
+            {
+                //getting media
+                var image = event.media; 
+                 
+                //checking if it is photo
+                if(event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO)
+                {
+                    //we may create image view with contents from image variable
+                    //or simply save path to image
+                    Ti.App.Properties.setString("image", image.nativePath);
+                }
+            },
+            cancel:function()
+            {
+                //do somehting if user cancels operation
+            },
+            error:function(error)
+            {
+                //error happend, create alert
+                var a = Titanium.UI.createAlertDialog({title:'Camera'});
+                //set message
+                if (error.code == Titanium.Media.NO_CAMERA)
+                {
+                    a.setMessage('Device does not have camera');
+                }
+                else
+                {
+                    a.setMessage('Unexpected error: ' + error.code);
+                }
+ 
+                // show alert
+                a.show();
+            },
+            allowImageEditing:true,
+            saveToPhotoGallery:true
+        });
+    }
+    else if(e.index == 1)
+    {
+        //obtain an image from the gallery
+        Titanium.Media.openPhotoGallery({
+            success:function(event)
+            {
+                //getting media
+                var image = event.media; 
+                // set image view
+                 
+                //checking if it is photo
+                if(event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO)
+                {
+                    //we may create image view with contents from image variable
+                    //or simply save path to image
+                    Ti.App.Properties.setString("image", image.nativePath);
+                    Ti.App.fireEvent('web:loadImage', { image: image.nativePath });
+                }   
+            },
+            cancel:function()
+            {
+                //user cancelled the action fron within
+                //the photo gallery
+            }
+        });
+    }
+    else
+    {
+        //cancel was tapped
+        //user opted not to choose a photo
+    }
+});
+ 
+//show dialog
+dialog.show();
